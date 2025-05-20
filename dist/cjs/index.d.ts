e99af669e43ebe2145ac50687f1aa559d7b8d2e0
Nawapat Buakoet <nawapat.b@covest.finance>
2025-05-20 16:14:27 +0700

    Build

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,58 @@
+import { Elysia } from 'elysia';
+import { type ContextManager, type Context, type SpanOptions, type Span, type Attributes, TraceAPI } from '@opentelemetry/api';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+type OpenTeleMetryOptions = NonNullable<ConstructorParameters<typeof NodeSDK>[0]>;
+/**
+ * Initialize OpenTelemetry SDK
+ *
+ * For best practice, you should be using preload OpenTelemetry SDK if possible
+ * however, this is a simple way to initialize OpenTelemetry SDK
+ */
+export interface ElysiaOpenTelemetryOptions extends OpenTeleMetryOptions {
+    contextManager?: ContextManager;
+}
+export type ActiveSpanArgs<F extends (span: Span) => unknown = (span: Span) => unknown> = [name: string, fn: F] | [name: string, options: SpanOptions, fn: F] | [name: string, options: SpanOptions, context: Context, fn: F];
+export type Tracer = ReturnType<TraceAPI['getTracer']>;
+export type StartSpan = Tracer['startSpan'];
+export type StartActiveSpan = Tracer['startActiveSpan'];
+export declare const contextKeySpan: unique symbol;
+export declare const getTracer: () => ReturnType<TraceAPI["getTracer"]>;
+export declare const startActiveSpan: StartActiveSpan;
+export declare const record: {
+    <F extends (span: Span) => unknown>(name: string, fn: F): ReturnType<F>;
+    <F extends (span: Span) => unknown>(name: string, options: SpanOptions, fn: F): ReturnType<F>;
+    <F extends (span: Span) => unknown>(name: string, options: SpanOptions, context: Context, fn: F): ReturnType<F>;
+};
+export declare const getCurrentSpan: () => Span | undefined;
+/**
+ * Set attributes to the current span
+ *
+ * @returns boolean - whether the attributes are set or not
+ */
+export declare const setAttributes: (attributes: Attributes) => boolean;
+export declare const opentelemetry: ({ serviceName, instrumentations, contextManager, ...options }?: ElysiaOpenTelemetryOptions) => Elysia<"", {
+    decorator: {};
+    store: {};
+    derive: {};
+    resolve: {};
+}, {
+    typebox: {};
+    error: {};
+}, {
+    schema: {};
+    standaloneSchema: {};
+    macro: {};
+    macroFn: {};
+    parser: {};
+}, {}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+}, {
+    derive: {};
+    resolve: {};
+    schema: {};
+    standaloneSchema: {};
+}>;
+export {};
